9e18a9c6770d9937f9919dbead9bff7dc682d8c0
Lucas Paiva <lpaiva234@gmail.com>
2025-03-27 11:03:56 -0300

    add hover effect for filling tiles

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let container = document.querySelector(".container");
-let n = 256;
+function createGrid(n) {
 
-for (let i = 0; i < n; i++) {
-    let tile = document.createElement("div");
-    tile.classList.add("tile");
-    container.appendChild(tile);
+    for (let i = 0; i < n * n; i++) {
+        let tile = document.createElement("div");
+        tile.classList.add("tile");
+        container.appendChild(tile);
+    }
+}
+
+function fillSquare(tile) {
+    tile.classList.add("filled");
+}
+
+let container = document.querySelector(".container");
+
+let n = 16;
+createGrid(n);
+
+let mousePressed = false;
+document.body.addEventListener("mousedown", () => {
+    mousePressed = true;
+})
+document.body.addEventListener("mouseup", () => {
+    mousePressed = false;
+})
+
+
+let tileList = document.querySelectorAll(".tile");
+for (let i = 0; i < tileList.length; i++) {
+    let currentTile = tileList[i];
+    currentTile.addEventListener("mouseover", () => {
+        if (mousePressed) {
+            fillSquare(currentTile);
+        }
+    });
 }
